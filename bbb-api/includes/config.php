47e6b7e174335c5492e1510d34f8795ecb352874
strongpapazola <strongpapazola@gmail.com>
2020-05-28 18:13:20 +0700

    a

--- bbb-api/includes/config.php
+++ bbb-api/includes/config.php
@@ -1,3 +1,3 @@
 <?php
-define("CONFIG_SECURITY_SALT", "txljeu6Wa3UNRpadsflsdgfakjsddhflkajdhs");
+define("CONFIG_SECURITY_SALT", "txljeu6Wa3UNRpadsflsdgfakjasdasd");
 define("CONFIG_SERVER_BASE_URL", "https://<maindomain>/bigbluebutton/");
